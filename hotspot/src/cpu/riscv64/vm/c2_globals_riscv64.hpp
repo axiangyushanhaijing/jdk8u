0bc3f718dc8caf43b096ab6c8bc3c03d2ad6f485
zhangxiang-plct <zhangxiang@iscas.ac.cn>
2022-11-15 15:27:56 +0800

    Add BackEdgeThreshold

--- hotspot/src/cpu/riscv64/vm/c2_globals_riscv64.hpp
+++ hotspot/src/cpu/riscv64/vm/c2_globals_riscv64.hpp
@@ -44,7 +44,7 @@ define_pd_global(bool, UseOnStackReplacement,        true);
 define_pd_global(bool, ProfileInterpreter,           true);
 define_pd_global(bool, TieredCompilation,            trueInTiered);
 define_pd_global(intx, CompileThreshold,             10000);
-
+define_pd_global(intx, BackEdgeThreshold,            100000);
 define_pd_global(intx, OnStackReplacePercentage,     140);
 define_pd_global(intx, ConditionalMoveLimit,         0);
 define_pd_global(intx, FLOATPRESSURE,                64);
